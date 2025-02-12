7b5dd7e6db947f8fe656b5076fe9322caba3037d
hemant <hemantkumar692@gmail.com>
2025-02-12 20:29:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import 'antd/dist/reset.css'; 
+import React from 'react';
+import { Button, Card } from 'antd';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Button type="primary">Button</Button>
   );
 }
 
