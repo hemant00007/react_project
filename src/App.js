547b18f1c43b83aaccbb0321b52c33a32d3fa79b
hemant <hemantkumar692@gmail.com>
2025-02-18 18:26:31 +0530

    Input example in react

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
 import 'antd/dist/reset.css'; 
 import React from 'react';
-import { Button, Card } from 'antd';
+import { Button, Card,Input } from 'antd';
 import { useState } from 'react';
-import { PoweroffOutlined, ProfileFilled } from '@ant-design/icons';
+import { HomeOutlined, PoweroffOutlined, ProfileFilled, UserOutlined } from '@ant-design/icons';
 import './App.css';
 
 
 function App() {
 
-  const [loading,setLoading] = useState(false);
-  const onButtonClick = (e) => {
-    console.log('Button clicked');
-    setLoading(true);
   
-    setTimeout(() => {
-      setLoading(false);
-  },2000);
-}
+
   
   return (
+   
     <div style={{ 
       display: 'flex',
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh', // This makes the container take the full viewport height
     }}>
-   <Button type="primary"
-   loading={loading} 
-   icon={<ProfileFilled />}
-   className="my-btn"
-  //  style={{backgroundColor: 'green',color:'pink'}}
-   onClick={onButtonClick}
-    >click</Button>
+
+
+<Input.TextArea
+placeholder="Enter your name"
+maxLength={10}
+prefix={<UserOutlined />}
+allowClear
+
+/>
+   
     </div>
+    
   );
 }
 
