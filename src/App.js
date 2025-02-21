cb48f6ffd8b77040f0e6bbb95e48a495f4bec667
hemant <hemantkumar692@gmail.com>
2025-02-21 19:02:16 +0530

    A Form in Ract

--- src/App.js
+++ src/App.js
@@ -8,30 +8,47 @@ import './App.css';
 
 function App() {
 
-  
-
-  
   return (
-   
-    <div style={{ 
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh', // This makes the container take the full viewport height
-    }}>
+
+    <div className="container">
+      <h1>Form In React</h1>
+      <form>
+        <label htmlFor='firstname'> First Name*</label>
+        <input type ='text' placeholder='Enter your first name' name="firstname"/>
+        <label htmlFor='lasttname'> Last Name</label>
+        <input type="text" placeholder ="Enter Your Last Name" name ="lastname"/>
+        <label htmlFor ="email">Email</label>
+        <input type ="email" placeholder="Enter your email" namme ="email"/>
+        <label htmlFor="mobile">Phone No</label>
+        <input type ="number" placeholder="Enter your phone no" name ="phone"/>
+        <label htmlFor="gender">Gender</label>
+        <input type="radio" name="gender"/> Male
+        <input type ="radio" name ="gender"/> Female
+        <input type ="radio" name="gender"/> Other
+        <label htmlFor="subject">Subject</label>
+        <select name="subject" id="subject">
+          <option value="math">Math</option>
+          <option value="hindi">Hindi</option>
+          <option value="english">English</option>
+        </select>
+        <label htmlFor="resume">Resume</label>
+        <input type="file" placeholder="Upload your resume" name="resume"/>
+        <label htmlFor="url">URL</label>
+        <input type="text" placeholder="Enter your Image URL" name="url"/>
+        <label htmlFor="about">About </label>
+       <textarea name="about" id="about" cols="30" rows="10" placeholder="Enter Description"></textarea>
+      <button type="button">Reset</button>
+      <button type="submit">Submit</button>
 
 
-<Input.TextArea
-placeholder="Enter your name"
-maxLength={10}
-prefix={<UserOutlined />}
-allowClear
 
-/>
-   
-    </div>
-    
-  );
+
+
+
+
+        </form>
+      </div >    
+  )
 }
 
 export default App;
